b7d6c1fabb09ae508977e2214ecc0b296d11da87
unknown <pratttrevor79@gmail.com>
2022-05-29 11:55:06 -0500

    adding functions to store and display user highscores and name

--- ASSETS/Java/script.js
+++ ASSETS/Java/script.js
@@ -1,5 +1,5 @@
 var timer = document.querySelector("#timer");
-var timerP = document.querySelector('header');
+var timerP = document.querySelector('header').children[1];
 var submitHighscore = document.querySelector("#submit-highscore");
 var viewHighscores = document.querySelector("#view-highscore");
 var clearHighscores = document.querySelector("#clearHighscores");
@@ -40,7 +40,7 @@ function startGame() {
 
 //timer function that runs while user takes quiz
 function startTimer() {
-    var timeInterval = setInterval(function() {
+    var timeInterval = setInterval(function () {
         if (gameEnd === true) {
             clearInterval(timeInterval);
             return;
@@ -57,7 +57,7 @@ function startTimer() {
 }
 
 //function that calls back to showQuestions to prompt questions 
-function showQuestions (currentQuestionIndex){
+function showQuestions(currentQuestionIndex) {
     titleTag.textContent = question.questions[currentQuestionIndex];
     createAnswersElement(currentQuestionIndex);
 
@@ -65,14 +65,14 @@ function showQuestions (currentQuestionIndex){
 }
 
 //creates a new answer element in the answer list element & clears out previous answer 
-function createAnswersElement(currentQuestionIndex){
+function createAnswersElement(currentQuestionIndex) {
     answerButtonList.innerHTML = '';
 
-    for(let answersIndex = 0; answersIndex < question.answers[currentQuestionIndex].length; answersIndex++){
+    for (let answersIndex = 0; answersIndex < question.answers[currentQuestionIndex].length; answersIndex++) {
         var currentAnswerListItem = document.createElement('li');
         var tempStr = (question.answers[currentQuestionIndex][answersIndex].includes('correct:'));
 
-        if(question.answers[currentQuestionIndex][answersIndex].includes('correct')){
+        if (question.answers[currentQuestionIndex][answersIndex].includes('correct')) {
             tempStr = question.answers[currentQuestionIndex][answersIndex].substring(8, question.answers[currentQuestionIndex][answersIndex].length);
             currentAnswerListItem.id = 'correct';
         }
@@ -85,12 +85,12 @@ function createAnswersElement(currentQuestionIndex){
 
 //function to switch to next question and show question content 
 
-function nextQuestion(){
+function nextQuestion() {
     questionsIndexNumber++;
-    if(questionsIndexNumber >= question.questions.length){
+    if (questionsIndexNumber >= question.questions.length) {
         endGame();
     }
-    else{
+    else {
         showQuestions(questionsIndexNumber);
     }
 
@@ -99,7 +99,7 @@ function nextQuestion(){
 
 //function to end the question "game"
 
-function endQuiz(){
+function endQuiz() {
     gameEnded = true;
     score = timeLeft;
 
@@ -113,8 +113,85 @@ function endQuiz(){
     document.querySelector('#submit-highscore-div').style.display = 'block';
 }
 
+function checkAnswer(event) {
+    if (event.target != answerButtonList) {
+
+        if (!(event.target.id.includes('correct'))) {
+            timeRemaining -= 10;
+        }
+
+        nextQuestion();
+    }
+
+    return;
+}
+
+function storeScoreAndName() {
+    var highscoreTextbox = document.querySelector(`input`);
+    var tempArrayOfObjects = [];
+    if (highscoreTextbox.value != `` || highscoreTextbox.value != null) {
+        var tempObject = {
+            names: highscoreTextbox.value,
+        }
+
+        if (window.localStorage.getItem(`highscores`) == null) {
+            tempArrayOfObjects.push(tempObject);
+            window.localStorage.setItem(`highscores`, JSON.stringify(tempArrayOfObjects));
+
+        } else {
+
+            for (let index = 0; index <= tempArrayOfObjects.length; index++) {
+                if (index == tempArrayOfObjects.length) {
+                    tempArrayOfObjects.push(tempObject)
+                    break;
+                } else if (tempArrayOfObjects[index].scores < score) {
+                    tempArrayOfObjects.splice(index, 0, tempObject);
+                    break;
+                }
+            }
+            window.localStorage.setItem(`highscores`, JSON.stringify(tempArrayOfObjects))
+        }
+        document.querySelector(`input`).value = ``;
+        score = 0;
+        showHighscores();
+    }
+
+    return;
+}
+
+
+function showHighscores() {
+    //elements needed to hide
+    titleTag.style.display = `none`; 
+    startBtn.style.display = `none`; 
+    document.querySelector(`header`).children[0].style.display = `none`; 
+    document.querySelector(`#instructions`).style.display = `none`; 
+    document.querySelector(`#submit-highscore`).style.display = `none`; 
+
+    //show highscore div and start filling it up
+    document.querySelector(`#view-highscore`).style.display = `block`; 
+
+    tempOrderedList = document.querySelector(`ol`); 
+    tempOrderedList.innerHTML = `` 
+
+    tempArrayOfObjects = JSON.parse(window.localStorage.getItem(`highscores`)); 
+    if (tempArrayOfObjects != null) { 
+        for (let index = 0; index < tempArrayOfObjects.length; index++) { 
+            var newLi = document.createElement(`li`) 
+            newLi.textContent = tempArrayOfObjects[index].names + ` - ` + tempArrayOfObjects[index].scores; 
+            tempOrderedList.appendChild(newLi);
+        }
+
+    } else { 
+        var newLi = document.createElement(`p`) 
+        newLi.textContent = `No Highscores`
+        tempOrderedList.appendChild(newLi); 
+    }
+
+    return;
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function quizStart(){
+function quizStart() {
     startQuiz.addEventListener('click', startGame);
     answerButtonList.addEventListener('click', checkAnswer);
     viewHighscores.addEventListener('click', showHighscores);
